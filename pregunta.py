84ace3999b3d0eb6bfe1fb35059ba973d027589a
David Mateo García Vallejo <80338757+davgarciava@users.noreply.github.com>
2022-11-15 09:19:58 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -11,10 +11,23 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
+    
+    df.dropna(axis = 0, inplace = True)
+    df.drop_duplicates(inplace = True)
 
-    #
-    # Inserte su código aquí
-    #
+    for columna in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'lÃ­nea_credito', 'barrio']:
+        df[columna] = df[columna].str.lower()
+        df[columna] = df[columna].apply(lambda x: x.replace('_', ' '))
+        df[columna] = df[columna].apply(lambda x: x.replace('-', ' '))
+
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace("\$[\s*]", "")
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(",", "")
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace("\.00", "")
+    df['monto_del_credito'] = df['monto_del_credito'].astype(int)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(float)
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
+    
+    df.drop_duplicates(inplace = True)
 
     return df
