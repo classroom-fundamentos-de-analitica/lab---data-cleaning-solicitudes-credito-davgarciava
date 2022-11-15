d64296d47ae06c2fcaf83fdb0a721b930544d4d8
David Mateo García Vallejo <80338757+davgarciava@users.noreply.github.com>
2022-11-15 09:26:08 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -1,24 +1,16 @@
-"""
-Limpieza de datos usando Pandas
------------------------------------------------------------------------------------------
+# Limpieza de datos usando Pandas
 
-Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada 
-correctamente. Tenga en cuenta datos faltantes y duplicados.
-
-"""
 import re
 import pandas as pd
 from datetime import datetime
 
-
 def clean_data():
-
     df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
     
     df.dropna(axis = 0, inplace = True)
     df.drop_duplicates(inplace = True)
 
-    for columna in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'lÃ­nea_credito', 'barrio']:
+    for columna in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
         df[columna] = df[columna].str.lower()
         df[columna] = df[columna].apply(lambda x: x.replace('_', ' '))
         df[columna] = df[columna].apply(lambda x: x.replace('-', ' '))
@@ -29,7 +21,7 @@ def clean_data():
     df['monto_del_credito'] = df['monto_del_credito'].astype(int)
     df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(float)
     df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
-    
+
     df.drop_duplicates(inplace = True)
 
     return df
